e2de3055bf744705db032d934f1cb2ff4e0648c9
Eric Yan <yanhuan25@gmail.com>
2017-08-31 12:35:08 +1000

    Add files via upload

---
+++ Octave Projects/3.Multi-class Classi-cation and Neural Networks/predict.m
@@ -0,0 +1,43 @@
+ bfunction p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+temp1=sigmoid(X*Theta1');
+temp2=[ones(size(temp1,1), 1) temp1];
+
+[temp3,p]=max(sigmoid(temp2*Theta2'),[],2);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
+
+
+
+
+
+
+
+
+
+% =========================================================================
+
+
+end
